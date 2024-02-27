5429bdf3ca6f391ae5ac00631cc311e5c3b15a7e
samiya1859 <samiya1859@gmail.com>
2024-02-27 16:12:09 +0600

    csrf link added

--- libraryManagement/settings.py
+++ libraryManagement/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://librarymanagementprojectdeploy.onrender.com', 'https://*.127.0.0.1'
+    'https://librarymanagementdeploying.onrender.com', 'https://*.127.0.0.1'
     ]
 # Application definition
 
